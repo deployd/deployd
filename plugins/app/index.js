6fd2f917e33cf7fddc83d2476e41c8cc580acb42
Ritchie Martori <ritchiem@terralever.com>
2011-10-06 08:18:03 -0700

    test coverage, user routes

--- plugins/app/index.js
+++
@@ -1,5 +0,0 @@
-var app = lib.require('app');
-
-app.get('/app/:id', function(req, res) {
-  res.send({app: req.param('id') || 0});
-});
--- plugins/user/users.js
+++
@@ -1,5 +0,0 @@
-var Collection = lib.require('collection');
-
-console.log(Collection.fetch);
-
-module.exports = Collection.spawn({collection: 'users'});
