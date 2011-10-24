79486f13268665b491831ea20f8eb857bfc5ee5c
Ritchie Martori <ritchiem@terralever.com>
2011-10-23 17:33:42 -0700

    100 % coverage on db, model

--- test/model.test.js
+++ test/model.test.js
@@ -5,44 +5,81 @@ var Model = require('model')
   , TestModel = Model.spawn({
       collection: 'tests',
     })
-  , data = {id: 1234, foo: 'bar', bat: 'baz'}
+  , random = Math.random().toString()
+  , data = {random: random, foo: 'bar', bat: 'baz'}
+  , updates = {random: random, foo: 'bat'}
+  , i = 0
 ;
-
 
 module.exports = {
   
-  'creating a new model': function(done, assert) {
-    var m = TestModel
+  'creating a new model': function(beforeExit, assert) {
+    var triedRemoving = false
+      , m = TestModel
       .spawn()
       .set(data)
       .notify(function(json) {
-        console.log('json', json);
+        var id = json._id;
+        TestModel.spawn().set({_id: id}).notify(function(json) {
+          assert.ok(json.random === random);
+          m
+            .set({random: 'foo'})
+            .notify(function(json) {
+              json.random && assert.ok(json.random == 'foo');
+              if(!triedRemoving) {
+                triedRemoving = true;
+                m.notify(function(json) {
+                  assert.ok(json.removed);
+                  m.notify(function(json) {
+                    assert.ok(!json._id);
+                  })
+                  .fetch();
+                })
+                .remove();
+              }
+            })
+            .save()
+          ;
+        }).fetch();
+        delete json._id;
         assert.eql(json, data);
+
       })
       .save()
     ;
-    console.log(m);
+    
+    assert.ok(m.status() === 'write', 'should be writing');
+    
+    beforeExit(function() {
+      assert.ok(triedRemoving);
+    });
+    assert.ok(m.isNew());
   },
   
-  // 'find and update': function(done, assert) {
-  //   TestModel
-  //     .spawn()
-  //     .set(data)
-  //     .notify(function(json) {
-  //         assert.eql(json, data);
-  //     })
-  //     .fetch()
-  //   ;
-  // },
-  // 
-  // 'remove': function(done, assert) {
-  //   TestModel
-  //     .spawn()
-  //     .set(data)
-  //     .notify(function(json) {
-  //       assert.ok(json.removed);
-  //     })
-  //   ;
-  // }
+  'find and update': function(done, assert) {
+    var m = TestModel
+      .spawn()
+      .set(updates)
+      .notify(function(json) {
+        assert.ok(++i < 2, 'should not call notify more than once!');
+        assert.eql(json.foo, updates.foo);
+        m.remove();
+      })
+      .save()
+    ;
+  },
+  
+  'look for something that isn\'t there': function(done, assert) {
+    TestModel
+      .spawn()
+      .set({baz: 'baz!!!!!'})
+      .notify({
+        send: function(json) {
+          assert.eql(json, [{"message":"Does not exist","type":"Not Found"}]);
+        }
+      })
+      .fetch()
+    ;
+  }
   
 }
