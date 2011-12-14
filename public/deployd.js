4ee6816509ad3b145c8703663c328a5b1bf7cc21
Ritchie Martori <ritchiem@terralever.com>
2011-12-14 08:25:48 -0800

    now supports jsonp

--- public/deployd.js
+++ public/deployd.js
@@ -1,6 +1,7 @@
 function d(route, data, callback) {
   var arg
     , i = 0
+    , host = d.host()
     , options = {
         dataType: 'json',
         contentType: 'application/json'
@@ -22,5 +23,34 @@ function d(route, data, callback) {
     }
   }
   
+  if(host && host !== window.location.host) {
+    options.url = window.location.protocol + '//' + host + options.url;
+    if(d.can('use cors')) {
+      options.beforeSend = function(xhr) {
+        xhr.withCredentials = true;
+      };
+    } else {
+      options.dataType = 'jsonp';
+    }
+  }
+  
   $.ajax(options);
 }
+
+d.host = function(host) {
+  host && (d._host = host);
+  return d._host || '';
+};
+
+d.can = function(feature) {
+  // cache test results
+  d.results = d.results || {};
+  d.tests = d.tests || {
+    'use cors': function() {
+      var fauxXHR = new XMLHttpRequest();
+      return typeof fauxXHR.withCredentials !== 'undefined';
+    }
+  };
+  
+  return d.results[feature] || (d.results[feature] = d.tests[feature]());
+};
