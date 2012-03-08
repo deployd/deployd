4eadbeecf6bc0984843947ce9e6ad82256dc7273
Ritchie Martori <skawful@gmail.com>
2012-03-07 17:52:11 -0700

    15/15 passing, added session support, user tests

--- test/users.tests.js
+++ test/users.tests.js
@@ -3,8 +3,12 @@ describe('Users', function(){
     it('should register a new user', function(done) {
       users.post(data.users[0], function (err, user) {
         expect(user._id).to.exist;
-        delete user._id;
-        expect(user).to.eql(data.users[0]);
+        expect(user.email).to.eql(data.users[0].email);
+        expect(user.password).to.not.exist;
+        
+        // for use in GET /users test
+        data.users[0]._id = user._id;
+        
         done(err);
       })
     })
@@ -19,7 +23,46 @@ describe('Users', function(){
   
   describe('POST /users/login', function(){
     it('should login if provided the correct credentials', function(done) {
-      users.use('/login').post(data.users[0], done);
+      users.use('/login').post(data.users[0], function (err, session, req, res) {
+        expect(session._id).to.have.length(24);
+        expect(session.user.password).to.not.exist;
+        expect(res.headers['set-cookie'][0].indexOf(session._id) > -1).to.equal(true);
+        done(err);
+      });
+    })
+  })
+  
+  describe('DELETE /users/logout', function(){
+    it('should logout the current user', function(done) {
+      // TODO fix mdoq-http bug - loses context if replace client with users
+      client.use('/users/logout').del(done);
+    })
+    
+    it('should return an error if trying to logout twice', function(done) {
+      client.use('/users/logout').del(function (err, body, req, res) {
+        expect(err).to.exist;
+        // TODO confirm cookie is gone - mdoq-http doesnt include res for del()
+        done();
+      });
+    })
+  })
+  
+  describe('GET /users', function(){
+    it('should return a user if an id is provided', function(done) {
+      client.use('/users').get({_id: data.users[0]._id}, function (err, user) {
+        expect(user).to.exist;
+        expect(user).to.have.length(1);
+        expect(user[0].password).to.not.exist;
+        done(err);
+      })
+    })
+    
+    it('should not return a user when an _id is not provided', function(done) {
+      client.use('/users').get(function (err, res) {
+        expect(err).to.exist;
+        expect(res).to.not.exist;
+        done();
+      })
     })
   })
 })
