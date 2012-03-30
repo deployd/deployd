8f16f16954b392a94a09f24cf98c08e6d60cc825
Dallon Feldner <dallonfdev@gmail.com>
2012-03-30 14:38:09 -0700

    Fixed date validation

--- test/validation.test.js
+++ test/validation.test.js
@@ -21,6 +21,22 @@ describe('Resource Actions', function(){
         done();
       })
     })
+
+    it('should post a date in a standard format', function(done) {
+      todos.post({title: 'foo', dateCompleted: '12/12/12'}, function(err, todo, req, res) {
+        expect(err).to.not.exist;
+        expect(new Date(todo.dateCompleted) - new Date('12/12/12')).to.equal(0);
+        done(err);
+      });
+    });
+
+    it('should return an error for an invalid date', function(done) {
+      todos.post({title: 'foo', dateCompleted: 'bad date'}, function(err, todo, req, res) {
+        expect(err).to.exist;
+        expect(err.errors.dateCompleted).to.exist;
+        done();
+      });
+    });
     
     it('should ignore properties outside the schema', function(done) {
       todos.post({title: 'foo', bat: 'baz'}, function (err, todo, req, res) {
