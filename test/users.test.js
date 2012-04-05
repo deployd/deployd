96ee3a61c9e2d3a0e79a1a76ee79a69a3487224e
Ritchie Martori <skawful@gmail.com>
2012-04-05 09:56:53 -0700

    fixed GET returning bad data for /login

--- test/users.test.js
+++ test/users.test.js
@@ -24,13 +24,22 @@ describe('Users', function(){
   })
   
   describe('POST /users/login', function(){
+    var login = users.use('/login');
+    
     it('should login if provided the correct credentials', function(done) {
-      users.use('/login').post({email: data.users[0].email, password: data.users[0].password}, function (err, session, req, res) {
+      login.post({email: data.users[0].email, password: data.users[0].password}, function (err, session, req, res) {
         expect(session._id).to.have.length(24);
         expect(session.user.password).to.not.exist;
         expect(res.headers['set-cookie'][0].indexOf(session._id) > -1).to.equal(true);
         done(err);
       });
+    })
+    
+    it('should not respond to a GET', function(done) {
+      login.get(function (err, res) {
+        expect(err).to.exist;
+        done();
+      })
     })
   })
   
