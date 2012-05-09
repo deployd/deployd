8092ba7d09e2c49381118cb44beb16cdf72ff170
Ritchie Martori <skawful@gmail.com>
2012-05-09 14:49:53 -0700

    added router tests, added default static files resource

--- test/router.test.js
+++ test/router.test.js
@@ -1,8 +1,31 @@
-// describe('Router', function() {
-//   it('should support multi-level resources', function(done) { 
-//     client.use('/admin/files').get(function(err, body, req, res) {
-//       expect(body).to.exist;
-//       done(err);
-//     });
-//   });
-// });
+/**
+ * Router Unit Test
+ */
+
+describe('Router Middleware', function(){
+
+  var router = require('mdoq').use(require('../lib/router'))
+  
+  it('should determine filename', function(done) {
+    router.use('/index.html').get(function () {
+      expect(this.req.filename).to.equal('index.html');
+      done();
+    })
+  })
+  
+  it('should determine nested filenames', function(done) {
+    router.use('/a/b/c/d/e/f/g/h/index.html').get(function () {
+      expect(this.req.filename).to.equal('index.html');
+      done();
+    })
+  })
+  
+  it('should determine the resource path', function(done) {
+    router.use('/').get(function () {
+      expect(this.req.resource.path).to.equal('/');
+      expect(this.req.resource.type).to.equal('Static');
+      done();
+    })
+  })
+  
+})
