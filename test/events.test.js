dbaa3d4077e4a6ddb12d7d4b70b076b2113812f0
Ritchie Martori <skawful@gmail.com>
2012-05-10 12:15:51 -0700

    renamed and moved onSave event, now onValidate

--- test/events.test.js
+++ test/events.test.js
@@ -4,6 +4,7 @@ describe('Resource Events', function(){
       todos.post({title: 'post event handler test'}, function (err, todo) {
         expect(todo._id).to.exist;
         expect(todo.isPost).to.equal(true);
+        expect(todo.isValidate).to.equal(true);
         done(err);
       })
     });
@@ -18,7 +19,8 @@ describe('Resource Events', function(){
         });
       });
     });
-  })
+  });
+  
   describe('GET /todos', function(){
     it('should execute the todos GET event handler', function(done) {
       todos.post({title: 'feed the dog'}, function (err) {
@@ -32,18 +34,23 @@ describe('Resource Events', function(){
       })
     })
   });
+  
   describe('PUT /todos', function(){
     it('should execute the todos PUT event handler', function(done) {
       todos.post({title: 'foo'}, function (e, t) {        
         todos.get(function (e, all) {        
           todos.get({_id: all[0]._id}).put({title: 'post event handler test'}, function (err, todo) {
             expect(todo.isPut).to.equal(true);
+            expect(todo.isValidate).to.equal(true);
             done(err);
           })  
         })  
       })
     })
-  })
+  });
+  
+  
+  
   describe('DELETE /todos', function(){
     it('should execute the todos DELETE event handler', function(done) {
       todos.post({title: 'dont delete'}, function (e, r) {
