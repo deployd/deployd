a9adf33ddda1b739a5fa77d55663e6ac3226da31
Ritchie Martori <skawful@gmail.com>
2012-06-05 09:19:37 -0700

    fixed test

--- test/resources.unit.js
+++ test/resources.unit.js
@@ -15,7 +15,7 @@ describe('resources', function(){
   describe('.build(store)', function(){
     it('should return a set of resource instances', function(done) {
       resources.build(store).find(function (err, resources) {
-        expect(resources).to.have.length(1);
+        expect(resources).to.have.length(4);
         expect(resources[0].properties).to.be.a('object');
         expect(resources[0] instanceof Collection).to.equal(true);
         done(err);
