327b01664fb490540fe18f322292f0441792b1e8
Ritchie Martori <skawful@gmail.com>
2012-07-02 18:25:55 -0700

    removed stray console.log

--- clib/dpd.js
+++ clib/dpd.js
@@ -85,7 +85,6 @@
 
   function encodeIfComplex(query) {
     if (isComplex(query)) {
-    	console.log(JSON.stringify(query));
       return encodeURI(JSON.stringify(query));
     } else if (query) {
       return $.param(query);
