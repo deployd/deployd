686423c96b88f83e2e9f30de7cdef029f4f23106
Ritchie Martori <skawful@gmail.com>
2012-09-12 13:14:48 -0700

    removed uncaughtException handling, only pause requests that have a Content-Length

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var Server = require('./lib/server');
+var Server = require('./lib/server')
+  , upgrade = require('doh').upgrade;
 
 /**
  * export a simple function that constructs a dpd server based on a config
  */
 
 module.exports = function (config) {
-	return new Server(config);
+  return new Server(config);
 };
