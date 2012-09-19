05b1736eb7c40d82cc7dc6df75dc144613f6b9dc
Ritchie Martori <skawful@gmail.com>
2012-09-19 12:02:28 -0700

    added shared stdio with monitor child process

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var Server = require('./lib/server')
   , upgrade = require('doh').upgrade
-  , Monitor = require('./lib/monitor')
-  , EventEmitter = require('events').EventEmitter;
+  , Monitor = require('./lib/monitor');
 
 /**
  * export a simple function that constructs a dpd server based on a config
@@ -17,57 +16,4 @@ module.exports = function (config) {
  * opt-in process monitoring support
  */
 
-module.exports.createMonitor = function (config) {
-  var keypress = require('keypress');
-  var monitor = new Monitor(__dirname + '/lib/start.js')
-    , server = new EventEmitter();
-    
-  keypress(process.stdin);
-  
-  server.options = config;
-    
-  server.listen = function () {
-    monitor.start(function (err, commands, restarting) {
-      commands.start(config, function (err) {
-        if(err) {
-          server.emit('error', err);
-        } else if(!restarting) {
-          server.emit('listening');
-        }
-      });
-      
-      monitor.child.on('message', function (data) {
-        if(data.moduleError) {
-          monitor.stop();
-        }
-      });
-    });
-    
-    return server;
-  };
-  
-  monitor.on('exit', function () {
-    console.log();
-    process.stdout.write('Press any key to restart... or q to quit: ');
-    
-    function keypress(key, e) {
-      if(e.ctrl) {
-        // allow ctr+c, z, etc
-        console.log();
-      } else if(key == 'q') {  
-        console.log();
-        process.exit();
-      } else {
-        monitor.start(true);
-      }
-      process.stdin.pause();
-      process.stdin.setRawMode(false);
-    }
-    
-    process.stdin.once('keypress', keypress);
-    process.stdin.setRawMode(true);
-    process.stdin.resume();
-  });
-
-  return server;
-};
+module.exports.createMonitor = Monitor.createMonitor;
