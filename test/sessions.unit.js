cfd2ca8910cc62cb85b30b695ce1deeeb9f26cf9
Andrei Alecu <aandrei03@gmail.com>
2015-01-14 02:15:00 +0200

    Fix(session): race condition crash
    Should not crash process when inserting the same session to the database.

--- test/sessions.unit.js
+++ test/sessions.unit.js
@@ -1,6 +1,7 @@
 var SessionStore = require('../lib/session').SessionStore
 	,	db = require('../lib/db')
-	, EventEmitter = require('events').EventEmitter;
+	, EventEmitter = require('events').EventEmitter
+	, sinon = require('sinon');
 
 describe('SessionStore', function() {
 	it('should bind sockets to sessions', function() {
@@ -72,6 +73,15 @@ describe('Session', function() {
 			fn(err, session);
 		});
 	}
+	
+	beforeEach(function () {
+		this.sinon = sinon.sandbox.create();
+	});
+	
+	afterEach(function () {
+		this.sinon.restore();
+	});
+
 
 	it('should make sockets available even before they exist', function(done) {
 		this.timeout(100);
@@ -96,6 +106,29 @@ describe('Session', function() {
 		});
 	});
 
+	it('should not crash process when inserting the same session to the database', function (done) {
+		var store = new SessionStore('sessions', db.create(TEST_DB));
+		var originalFind = store.find;
+		store.find = sinon.stub(store, "find", function () {
+			var myArgs = arguments;
+			// simulate 10 ms latency for find function
+			setTimeout(function() { originalFind.apply(store, myArgs); }, 10);
+		});
+			
+		var callsLeft = 5;
+		function callback(err) {
+			expect(err).to.not.exist;
+			callsLeft--;
+			if (callsLeft == 0) return done();
+		} 
+		
+		for (var i = 0; i< 5; i++) {
+			setTimeout(function() {
+				store.createSession("1c829fa9ed135d0301919aa70505cf36a39b35527b775e45e1523c354e0c5f2b0b1636f173a94e26082291ae9ad0e3e74cb5226bd0aa86ee9f7a3749d57cc74d", callback);
+			}, 1);
+		}
+	});
+		
 	describe('.set(changes)', function() {
 		it('should set the changes to a sessions data', function(done) {
 			createSession(function (err, session) {
