9b854ab0e391053b04fe73494f53a5b71e8c694e
Andrei Alecu <aandrei03@gmail.com>
2015-01-31 13:29:33 +0200

    Change method of running dpd CLI from fork to spawn to workaround the child process not being able to handle process.exit event.

--- test-app/runtests.js
+++ test-app/runtests.js
@@ -1,11 +1,12 @@
 var fork = require('child_process').fork
+  , spawn = require('child_process').spawn
   , shelljs = require('shelljs')
   , fs = require('fs');
 
 
 if (!fs.existsSync('app.dpd')) {
   console.log('Not a deployd app directory, please run this from a deployd app directory');
-  process.exit();
+  process.exit(1);
   return;
 }
 
@@ -16,10 +17,14 @@ if (fs.existsSync('data')) {
   console.log('Removing previous data directory');
   shelljs.rm('-rf', 'data');
 }
-var proc = fork("../bin/dpd", [], {silent: true})
+
+// using `spawn` because with `fork` the child script won't be able to catch a `process.exit()` event
+// thus leaving mongod zombie processes behind. see https://github.com/joyent/node/issues/5766
+var proc = spawn(process.argv[0], ["../bin/dpd"], {env: process.env})
   , buf = '';
 
-proc.on("error", function() {
+proc.on("error", function(err) {
+  console.error(err);
   process.exit(1);
 });
 
@@ -45,8 +50,14 @@ function kill(e) {
 
 proc.once('listening', function (port){
   var mpjsProc = fork('../node_modules/mocha-phantomjs/bin/mocha-phantomjs', [ '--ignore-resource-errors', 'http://localhost:' + port ], {silent: true});
+  mpjsProc.on("error", function(err) { 
+    console.error(err);
+  });
   mpjsProc.stdout.on('data', function(data) {
     process.stdout.write(data.toString());
+  });
+  mpjsProc.stderr.on('data', function(data) {
+    process.stderr.write(data.toString());
   });
   mpjsProc.on('exit', kill);
 })
