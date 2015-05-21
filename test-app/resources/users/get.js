74f2363141defd66b969a8ad84e2a0932f5c57ff
Andrei Alecu <andreialecu@users.noreply.github.com>
2015-05-21 20:28:02 +0300

    fix(UserCollection): `onGet` event can break session handling
    Fixes https://github.com/deployd/deployd/issues/565
    
    Some refactoring

--- test-app/resources/users/get.js
+++ test-app/resources/users/get.js
@@ -1,3 +1,8 @@
 dpd.users.get({id: {$in: this.friends}}, function(friends) {
     if (friends) this.friends = friends;
 });
+
+// keep here to verify issue:
+// https://github.com/deployd/deployd/issues/565
+if (!query.showUsername)
+  hide('username');
