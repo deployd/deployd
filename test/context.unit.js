5cbda30267bc6dae0a50e445d6950fa537528b12
Andrei Alecu <andreialecu@users.noreply.github.com>
2017-12-15 16:20:42 +0200

    fix: don't overwrite http headers with defaults
    This allows calling `ctx.res.setHeader('Content-Type', sometype)` from script code and ensuring deployd doesn't overwrite it.

--- test/context.unit.js
+++ test/context.unit.js
@@ -15,4 +15,36 @@ describe('Context', function() {
       });
     });  
   });
+
+  describe('.done', function() {
+    it('should provide default headers', function(done) {
+      freq('/foo/bar', null, function(req, res) {
+        var r = new Resource('foo', {});
+        var ctx = new Context(r, req, res, {});
+        
+        var response = 'callback(123)';
+
+        ctx.done(null, response);
+        expect(ctx.res.getHeader('Content-Type')).to.equal('text/html; charset=utf-8');
+        expect(ctx.res.getHeader('Cache-Control')).to.equal('no-cache, no-store, must-revalidate');
+        expect(ctx.res.getHeader('Pragma')).to.equal('no-cache');
+        expect(ctx.res.getHeader('Expires')).to.equal('0');
+        expect(ctx.res.getHeader('Content-Length')).to.equal(response.length);
+
+        done();
+      });
+    });  
+
+    it('should not overwrite headers', function(done) {
+      freq('/foo/bar', null, function(req, res) {
+        var r = new Resource('foo', {});
+        var ctx = new Context(r, req, res, {});
+        
+        ctx.res.setHeader('Content-Type', 'text/javascript');
+        ctx.done(null, 'callback(123)');
+        expect(ctx.res.getHeader('Content-Type')).to.equal('text/javascript');
+        done();
+      });
+    });  
+  });
 });
