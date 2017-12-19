c6d8fdba44dea6b0e38c6ec2f0774af216197783
Andrei Alecu <andreialecu@users.noreply.github.com>
2017-12-19 13:08:57 +0200

    fix: don't overwrite http headers with defaults (#851)
    This allows calling `ctx.res.setHeader('Content-Type', sometype)` from script code and ensuring deployd doesn't overwrite it.
    
    It will also prevent deployd from ending the response a second time if an event script calls `ctx.res.end(...)`. thus preventing an error from being logged.

--- test/context.unit.js
+++ test/context.unit.js
@@ -1,6 +1,7 @@
 var Resource = require('../lib/resource')
   , Context = require('../lib/context')
-  , Stream = require('stream').Stream;
+  , Stream = require('stream').Stream
+  , sinon = require('sinon');
 
 describe('Context', function() {
 
@@ -11,6 +12,65 @@ describe('Context', function() {
         var ctx = new Context(r, req, res, {});
 
         expect(ctx.url).to.equal('/bar');
+        done();
+      });
+    });  
+  });
+
+  describe('.done', function() {
+    it('should provide default headers', function(done) {
+      freq('/foo/bar', null, function(req, res) {
+        var r = new Resource('foo', {});
+        var ctx = new Context(r, req, res, {});
+        
+        var response = 'callback(123)';
+
+        ctx.done(null, response);
+        expect(ctx.res.getHeader('Content-Type')).to.equal('text/html; charset=utf-8');
+        expect(ctx.res.getHeader('Cache-Control')).to.equal('no-cache, no-store, must-revalidate');
+        expect(ctx.res.getHeader('Pragma')).to.equal('no-cache');
+        expect(ctx.res.getHeader('Expires')).to.equal('0');
+        expect(ctx.res.getHeader('Content-Length')).to.equal(response.length);
+
+        done();
+      });
+    });  
+
+    it('should not overwrite headers', function(done) {
+      freq('/foo/bar', null, function(req, res) {
+        var r = new Resource('foo', {});
+        var ctx = new Context(r, req, res, {});
+        
+        ctx.res.setHeader('Content-Type', 'text/javascript');
+        ctx.done(null, 'callback(123)');
+        expect(ctx.res.getHeader('Content-Type')).to.equal('text/javascript');
+        done();
+      });
+    });
+
+    it('should not send headers if they were already sent', function(done) {
+      freq('/foo/bar', null, function(req, res) {
+        var r = new Resource('foo', {});
+        var ctx = new Context(r, req, res, {});
+        ctx.res.writeHead(200);
+        expect(ctx.res.headersSent).to.be.true;
+        ctx.res.setHeader = sinon.spy();
+        ctx.done(null, 'callback(123)');
+        expect(ctx.res.setHeader.callCount).to.equal(0);
+        done();
+      });
+    });
+
+    it('should not call res.end() if response already finished', function(done) {
+      freq('/foo/bar', null, function(req, res) {
+        var r = new Resource('foo', {});
+        var ctx = new Context(r, req, res, {});
+        ctx.res.end("test");
+        expect(ctx.res.headersSent).to.be.true;
+        expect(ctx.res.finished).to.be.true;
+        ctx.res.end = sinon.spy();
+        ctx.done(null, 'callback(123)');
+        expect(ctx.res.end.callCount).to.equal(0);
         done();
       });
     });  
