68b018c9f6354ce59f43f153cf50703dd8eac169
Andrei Alecu <andrei.alecu@vecttor.co>
2019-03-11 18:38:45 +0200

    Fix identation

--- test/util.unit.js
+++ test/util.unit.js
@@ -26,125 +26,125 @@ describe('http', function() {
 		it('should parse a json query string', function() {
 			var q = http.parseQuery('/foo/bar?{"foo":"bar"}');
 			expect(q).to.eql({foo:'bar'});
-    });
-    
-    it('should not change a big number', function() {
-      var q = http.parseQuery('/foo/bar?id=9979174442646823');
-      // this number is expected to be changed by parseInt to 9979174442646824
+		});
+
+		it('should not change a big number', function() {
+			var q = http.parseQuery('/foo/bar?id=9979174442646823');
+			// this number is expected to be changed by parseInt to 9979174442646824
 			expect(q).to.eql({id:'9979174442646823'});
-    });
+		});
 	});
 
 describe('.getBody', function(){
 
-  it('should get body from stream', function(done){
+	it('should get body from stream', function(done){
 
-    var obj = JSON.stringify({foo: 'bar'})
-      , req = new Stream();
+		var obj = JSON.stringify({foo: 'bar'})
+			, req = new Stream();
 
-    var res = this.res;
+		var res = this.res;
 
-    http.getBody(req, function(buffer) {
-      expect(buffer).to.exist;
-      expect(buffer).to.eql(obj);
-      done();
-    });
+		http.getBody(req, function(buffer) {
+			expect(buffer).to.exist;
+			expect(buffer).to.eql(obj);
+			done();
+		});
 
-    req.emit('data', obj);
-    req.emit('end');
+		req.emit('data', obj);
+		req.emit('end');
 
-  });
+	});
 
-  it('should get body from rawBody', function(done){
+	it('should get body from rawBody', function(done){
 
-    var obj = JSON.stringify({foo: 'bar'})
-      , req = new Stream();
+		var obj = JSON.stringify({foo: 'bar'})
+			, req = new Stream();
 
-    req.rawBody = '';
-    req.on('data', function(chunk){
-      req.rawBody += chunk;
-    });
+		req.rawBody = '';
+		req.on('data', function(chunk){
+			req.rawBody += chunk;
+		});
 
-    var res = this.res;
+		var res = this.res;
 
-    req.on('end', function(){
-      http.getBody(req, function(buffer) {
-        expect(buffer).to.exist;
-        expect(buffer).to.eql(obj);
-        done();
-      });
-    });
+		req.on('end', function(){
+			http.getBody(req, function(buffer) {
+				expect(buffer).to.exist;
+				expect(buffer).to.eql(obj);
+				done();
+			});
+		});
 
-    req.emit('data', obj);
-    req.emit('end');
+		req.emit('data', obj);
+		req.emit('end');
 
-  });
+	});
 
 });
 
 describe('.parseBody()', function() {
-  beforeEach(function () {
-    this.res = {
-      setHeader: function () {
+	beforeEach(function () {
+		this.res = {
+			setHeader: function () {
 
-      }
-    };
-  });
+			}
+		};
+	});
 
-  it ('should parse json', function(done) {
-    var obj = {foo: 'bar'}
-      , req = new Stream();
+	it ('should parse json', function(done) {
+		var obj = {foo: 'bar'}
+			, req = new Stream();
 
-    http.parseBody(req, this.res, 'application/json', function(err, result) {
-      expect(err).to.not.exist;
-      expect(req.body).to.eql(obj);
-      done();
-    });
-    req.emit('data', JSON.stringify(obj));
-    req.emit('end');
-  });
+		http.parseBody(req, this.res, 'application/json', function(err, result) {
+			expect(err).to.not.exist;
+			expect(req.body).to.eql(obj);
+			done();
+		});
+		req.emit('data', JSON.stringify(obj));
+		req.emit('end');
+	});
 
-  it('should parse json in chunks', function(done) {
-    var req = new Stream()
-      , chunks = ['{"fo'
-                , 'o": "bar"'
-                , ', "bar"'
-                , ': "baz"'
-                , '}'];
+	it('should parse json in chunks', function(done) {
+		var req = new Stream()
+			, chunks = ['{"fo'
+								, 'o": "bar"'
+								, ', "bar"'
+								, ': "baz"'
+								, '}'];
 
-    http.parseBody(req, this.res, 'application/json', function(err) {
-      expect(err).to.not.exist;
-      expect(req.body).to.eql({"foo": "bar", "bar": "baz"});
-      done();
-    });
-    chunks.forEach(function(c) {
-      req.emit('data', c);
-    });
-    req.emit('end');
-  });
+		http.parseBody(req, this.res, 'application/json', function(err) {
+			expect(err).to.not.exist;
+			expect(req.body).to.eql({"foo": "bar", "bar": "baz"});
+			done();
+		});
+		chunks.forEach(function(c) {
+			req.emit('data', c);
+		});
+		req.emit('end');
+	});
 
-  it('should parse a form url-encoded value', function(done) {
-    var value = "foo=bar&bar=baz"
-      , req = new Stream();
+	it('should parse a form url-encoded value', function(done) {
+		var value = "foo=bar&bar=baz"
+			, req = new Stream();
 
-    http.parseBody(req, this.res, 'application/x-www-form-urlencoded', function(err) {
-      expect(err).to.not.exist;
-      expect(req.body).to.eql({"foo": "bar", "bar": "baz"});
-      done();
-    });
-    req.emit('data', value);
-    req.emit('end');
-  });
+		http.parseBody(req, this.res, 'application/x-www-form-urlencoded', function(err) {
+			expect(err).to.not.exist;
+			expect(req.body).to.eql({"foo": "bar", "bar": "baz"});
+			done();
+		});
+		req.emit('data', value);
+		req.emit('end');
+	});
 
-  it('should interpret an empty body as an empty object', function(done) {
-    var req = new Stream();
+	it('should interpret an empty body as an empty object', function(done) {
+		var req = new Stream();
 
-    http.parseBody(req, this.res, 'application/json', function(err) {
-      expect(err).to.not.exist;
-      expect(req.body).to.eql({});
-      done();
-    });
-    req.emit('end');
-  });
+		http.parseBody(req, this.res, 'application/json', function(err) {
+			expect(err).to.not.exist;
+			expect(req.body).to.eql({});
+			done();
+		});
+		req.emit('end');
+	});
 });
 });
